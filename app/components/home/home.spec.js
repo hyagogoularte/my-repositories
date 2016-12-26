936ba23502dc1fed1ed64e290799c7ef2b8319fd
Hyago Silva Goularte <hyagogoularte@gmail.com>
2016-12-26 00:36:01 -0200

    Adjusted input size, because is breaking

--- app/components/home/home.spec.js
+++ app/components/home/home.spec.js
@@ -1,35 +1,44 @@
-// describe( 'home section', function() {
+describe('HomeController', function() {
+    describe('isCurrentUrl', function() {
+        var HomeController, $scope, rootScope, httpBackend;
 
-//   var controller, rootScope, httpBackend;
+        beforeEach(module('pascalprecht.translate'));
+        beforeEach(module('application'));
 
-//   beforeEach(module('pascalprecht.translate'));
-//   beforeEach(module('application'));
+        beforeEach(inject(function($controller, $rootScope, _$translate_, _$httpBackend_) {
+            $scope = $rootScope.$new();
+            $translate = _$translate_;
+            HomeController = $controller('components.home.HomeController', {
+                $scope: $scope,
+                $translate: $translate
+            });
 
+            $httpBackend = _$httpBackend_;
+        }));
 
-//   beforeEach( module( function( $translateProvider ) {
-//     $translateProvider.registerAvailableLanguageKeys(['pt_BR', 'en_EN']);
-//   }));
+        it('should pass a dummy test', function() {
+            expect(HomeController).toBeTruthy();
+        });
 
-//   beforeEach(inject( function($injector){
-//       controller = $injector.get('$controller');
-//       rootScope = $injector.get('$rootScope');
-//       httpBackend = $injector.get('$httpBackend');
-//   }));
+   /*     it('should switch the translation from portuguese to english', function() {
+            $httpBackend.when('GET', 'assets/locale/locale-en_EN.json').respond(200);
+            spyOn($translate, 'use');
 
-//   it( 'should have a predefined language after start', inject(function($translate) {
-//     expect($translate.use()).toEqual('pt_BR');
-//   }));
+            HomeController.onChooseLang('en_EN');
+            expect($translate.use).toHaveBeenCalledWith('en_EN');
+        });
 
-//   it( 'should switch the translation from english to german', inject(function($translate) {
-//     httpBackend.when('GET', 'assets/locale/locale-en_EN.json').respond(200);
-//     spyOn($translate, 'use');
-  
-//     var applicationController = controller('ApplicationController', {});
-  
-//     applicationController.toggleLang();
-  
-//     expect($translate.use).toHaveBeenCalledWith('en_EN');
-//   }));
+        it('should have a predefined language after start', function() {
+            expect($translate.use()).toEqual('pt_BR');
+        });
 
+        it('should switch the translation from portuguese to english', function() {
+            $httpBackend.when('GET', 'assets/locale/locale-en_EN.json').respond(200);
+            spyOn($translate, 'use');
 
-// });
+            HomeController.onChooseLang('en_EN');
+            expect($translate.use).toHaveBeenCalledWith('en_EN');
+        });
+*/
+    });
+});
